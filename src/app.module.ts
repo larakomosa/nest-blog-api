483ad8f1f404c8479ad8885fa57e0b7764019983
Lara Komosa <lara.komosa@lt036.attlocal.net>
2021-02-03 21:37:12 -0600

    configuration completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        DatabaseModule,
+        UsersModule,
+    ]
 })
-export class AppModule {}
+export class AppModule { }
