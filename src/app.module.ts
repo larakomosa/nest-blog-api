d491f321ceb428354816da0e9947f36a5bcfca5b
Lara Komosa <lara.komosa@lt036.attlocal.net>
2021-02-04 13:25:05 -0600

    completing authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { AppController} from './app.controller'
+import {AppService} from './app.service'
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         DatabaseModule,
         UsersModule,
-    ]
+        AuthModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule { }
